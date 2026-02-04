94b93793661fc1482826c70eacffd9005256e987
Ian Zepp <ian.zepp@protonmail.com>
2026-02-04 16:38:55 -0500

    Initial tap with abbot formula

---
+++ Formula/abbot.rb
@@ -0,0 +1,34 @@
+class Abbot < Formula
+  desc "Persistent AI background daemon"
+  homepage "https://github.com/ianzepp/abbot"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/ianzepp/abbot-releases/releases/download/v0.1.0/abbot-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_ARM64_SHA256"
+    else
+      url "https://github.com/ianzepp/abbot-releases/releases/download/v0.1.0/abbot-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_X86_64_SHA256"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/ianzepp/abbot-releases/releases/download/v0.1.0/abbot-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
+    else
+      url "https://github.com/ianzepp/abbot-releases/releases/download/v0.1.0/abbot-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER_LINUX_X86_64_SHA256"
+    end
+  end
+
+  def install
+    bin.install "abbot"
+  end
+
+  test do
+    assert_match "abbot", shell_output("#{bin}/abbot --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,33 @@
+# Homebrew Tap
+
+Custom Homebrew formulae.
+
+## Installation
+
+```bash
+brew tap ianzepp/tap
+```
+
+## Formulae
+
+### abbot
+
+Persistent AI background daemon.
+
+```bash
+brew install ianzepp/tap/abbot
+```
+
+After installation:
+
+```bash
+# Show system info
+abbot info
+
+# Install as system service
+abbot service install
+abbot service start
+
+# Or run in foreground
+abbot run
+```
