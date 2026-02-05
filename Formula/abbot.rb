7061437f004a7e78d7c41de899d555e7ea7bc566
Ian Zepp <ian.zepp@protonmail.com>
2026-02-04 20:30:17 -0500

    Update abbot formula with v0.1.0 SHA256 checksums

--- Formula/abbot.rb
+++ Formula/abbot.rb
@@ -7,17 +7,17 @@ class Abbot < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ianzepp/abbot-releases/releases/download/v0.1.0/abbot-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      sha256 "234e2a8e08620945c3ac03922bfe1866f2cce046d11fa98f65a99674d4a834de"
     else
       url "https://github.com/ianzepp/abbot-releases/releases/download/v0.1.0/abbot-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_X86_64_SHA256"
+      sha256 "2b7ba22416550b2e2b476c643d6c126fe773b1895b5f5fc3327638f66759d4df"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/ianzepp/abbot-releases/releases/download/v0.1.0/abbot-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_X86_64_SHA256"
+      sha256 "c445c1dc359a4b6ab7b60a1224161f3b0e7b6af349402f8ccfd7d360204fccb2"
     end
     # Linux ARM64 not yet supported
   end
