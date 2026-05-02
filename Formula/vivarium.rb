596eae26d9231d3992a637e3b54609a0e8494cfd
Ian Zepp <ian.zepp@protonmail.com>
2026-05-02 09:32:59 -0400

    Update vivarium to 1.0.3

--- Formula/vivarium.rb
+++ Formula/vivarium.rb
@@ -5,19 +5,19 @@ class Vivarium < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ianzepp/vivarium/releases/download/v1.0.2/vivarium-aarch64-apple-darwin.tar.gz"
-      sha256 "8245004bf987669ff75fdd615eb1a430fc8eb1390ec8ec32a02ce67acb411cc2"
+      url "https://github.com/ianzepp/vivarium/releases/download/v1.0.3/vivi-aarch64-apple-darwin.tar.gz"
+      sha256 "9c4a9582e5a5ac0c19860a1ff00ad57c67e91705936f9da2b79c69bcc19ec8ab"
     else
-      url "https://github.com/ianzepp/vivarium/releases/download/v1.0.2/vivarium-x86_64-apple-darwin.tar.gz"
-      sha256 "82b5606eca312280df3ddec3f94d4a24421b4c439657b13853469e00a571e083"
+      url "https://github.com/ianzepp/vivarium/releases/download/v1.0.3/vivi-x86_64-apple-darwin.tar.gz"
+      sha256 "446f752702776d8b3a7e3499a311ae4529d8c8959923b554245639e22f7acf7e"
     end
   end
 
   def install
-    bin.install "vivarium"
+    bin.install "vivi"
   end
 
   test do
-    assert_match "vivarium", shell_output("#{bin}/vivarium --help")
+    assert_match "vivi", shell_output("#{bin}/vivi --help")
   end
 end
