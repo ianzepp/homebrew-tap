27f8e370c1b4255262bd8681fd8804337c8dd21b
Ian Zepp <ian.zepp@protonmail.com>
2026-06-03 17:05:27 -0400

    Update faber to v0.36.0

--- Formula/faber.rb
+++ Formula/faber.rb
@@ -4,11 +4,11 @@ class Faber < Formula
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/ianzepp/faber/releases/download/v0.35.0/faber-v0.35.0-aarch64-apple-darwin.tar.gz"
-    sha256 "d044200389eff8cc6a3933fda2c302c6f4a3786be6b7d0a9e0b3ef849ab87821"
+    url "https://github.com/ianzepp/faber/releases/download/v0.36.0/faber-v0.36.0-aarch64-apple-darwin.tar.gz"
+    sha256 "76dd9826a727a5f01e45e9616aebd3b5f6702220c22103f98dd82610393bdede"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ianzepp/faber/releases/download/v0.35.0/faber-v0.35.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "f1d0c1da63a961d1343832533e0bdb507010ba0d50948ed7d592c4c903cc4807"
+    url "https://github.com/ianzepp/faber/releases/download/v0.36.0/faber-v0.36.0-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "4da8d0486680d3f8469ffb02bd29eebde6d7761edca99f7a7da5fc9bbf68181e"
   else
     odie "Unsupported platform"
   end
@@ -18,7 +18,7 @@ class Faber < Formula
   end
 
   test do
-    assert_match "faber 0.35.0", shell_output("#{bin}/faber --version")
+    assert_match "faber 0.36.0", shell_output("#{bin}/faber --version")
     assert_match "Usage: faber <COMMAND>", shell_output("#{bin}/faber --help")
   end
 end
