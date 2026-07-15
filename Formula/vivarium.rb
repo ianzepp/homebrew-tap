81cc3a3ee01d19182f3377dc897257d2ba1d66f8
Ian Zepp <ian.zepp@protonmail.com>
2026-07-14 23:05:46 -0400

    Update vivarium to 5.4.0

--- Formula/vivarium.rb
+++ Formula/vivarium.rb
@@ -5,11 +5,11 @@ class Vivarium < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ianzepp/vivarium/releases/download/v5.3.0/vivi-aarch64-apple-darwin.tar.gz"
-      sha256 "996d38713c89d63b402d92e1e70fc436f2ea571f07b4ad924dfd10799671147f"
+      url "https://github.com/ianzepp/vivarium/releases/download/v5.4.0/vivi-aarch64-apple-darwin.tar.gz"
+      sha256 "9d5d6f5cc576b1321867d22a0d39679402c0eab28d847feae95d1f5e1aacea4a"
     else
-      url "https://github.com/ianzepp/vivarium/releases/download/v5.3.0/vivi-x86_64-apple-darwin.tar.gz"
-      sha256 "43e98aa3d6ca0217ddc4103644f0193d3a1b884735852bf750d5595cf24cb84c"
+      url "https://github.com/ianzepp/vivarium/releases/download/v5.4.0/vivi-x86_64-apple-darwin.tar.gz"
+      sha256 "e040cdca93e7060778c034ab00d7b6d052a4a7f1919f8f9355446d0b8c32bde7"
     end
   end
 
