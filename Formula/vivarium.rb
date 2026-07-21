9b968dc7cdeac44c95f88b952e3ab103f8113dde
Ian Zepp <ian.zepp@protonmail.com>
2026-07-21 11:58:26 -0400

    Update vivarium to 6.2.0

--- Formula/vivarium.rb
+++ Formula/vivarium.rb
@@ -2,14 +2,15 @@ class Vivarium < Formula
   desc "Local-first IMAP email sync for LLMs"
   homepage "https://github.com/ianzepp/vivarium"
   license "MIT"
+  version "6.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ianzepp/vivarium/releases/download/v6.1.0/vivi-aarch64-apple-darwin.tar.gz"
-      sha256 "9539e704643f586a55146960221009af9597ba544b385e68a63590d54d600a52"
+      url "https://github.com/ianzepp/vivarium/releases/download/v6.2.0/vivi-aarch64-apple-darwin.tar.gz"
+      sha256 "200130150b9e6ccb78afd1f1c338f703c433484fae7241ad394c4906bfb3b058"
     else
-      url "https://github.com/ianzepp/vivarium/releases/download/v6.1.0/vivi-x86_64-apple-darwin.tar.gz"
-      sha256 "82656ff5fde0fc2d1543e691db250d0aca12a91c29f414af06fa7edd21d0344f"
+      url "https://github.com/ianzepp/vivarium/releases/download/v6.2.0/vivi-x86_64-apple-darwin.tar.gz"
+      sha256 "03c2c1ed86d9701223e902ad1d684bfd5605ded13fe7647a239dbb8f8e4e7cde"
     end
   end
 
